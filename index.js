4d3581a773ea7585be5b67ba8e4f8e704e91dad3
savlana <108239589+savlana@users.noreply.github.com>
2022-07-16 17:48:33 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const myName = "Svitlana";
+ console.log(myName);
+ 
+
+
+
+ const mySurname = "Abelchuk";
+ console.log(mySurname);
+ 
+
+
+
+ let myFavoriteDrink = "Water with lime";
+ console.log(myFavoriteDrink);
+ 
+
+
+
+ let myFavoriteAnimal = "Cat";
+ console.log(myFavoriteAnimal);
+ 
+
+
+
+ let myFavoriteProgrammingLanguage = "JavaScript";
+ console.log(myFavoriteProgrammingLanguage);
